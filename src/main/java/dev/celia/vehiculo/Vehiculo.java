9783948f73b15a024a23274c7a1cd66318926018
Celia Garrido <celiagarridoherrera@gmail.com>
2025-03-27 10:49:26 +0100

    Clase Vehiculo creada, test Vehiculo falla falta implementacion Clases de los vehiculos

---
+++ src/main/java/dev/celia/vehiculo/Vehiculo.java
@@ -0,0 +1,16 @@
+package dev.celia.vehiculo;
+
+public abstract class Vehiculo {
+    protected String placa;
+
+    public Vehiculo(String placa) {
+        this.placa = placa;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public abstract double calcularPeaje();
+
+}
