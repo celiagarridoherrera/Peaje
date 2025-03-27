88ce1884fab7c616714af4242d820ba282877de3
Celia Garrido <celiagarridoherrera@gmail.com>
2025-03-27 10:58:54 +0100

    Creado test para Coche - TestCocheGuardaPlaca falla

--- src/test/java/dev/vehiculo/CocheTest.java
+++ src/test/java/dev/vehiculo/CocheTest.java
@@ -1,5 +1,22 @@
 package dev.vehiculo;
 
+import org.junit.jupiter.api.Test;
+
 public class CocheTest {
 
+    @Test
+    void testCochePaga100() {
+
+        Coche coche = new Coche("1234ABC");
+        
+        double peaje = coche.calcularPeaje();
+        assertEquals(100, peaje);
+        
+    }
+
+    @Test
+    void testCocheGuardaPlaca() {   
+            Coche coche = new Coche("1234ABC");
+            assertEquals("1234ABC", coche.getPlaca());
+        }
 }
