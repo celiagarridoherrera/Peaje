c793391cf6ae75e38c0403e57afdc17c8555c23f
Celia Garrido <celiagarridoherrera@gmail.com>
2025-03-28 12:36:21 +0100

    creado test para registrar vehiculo moto, test falla

--- src/test/java/dev/celia/estacionpeaje/EstacionPeajeTest.java
+++ src/test/java/dev/celia/estacionpeaje/EstacionPeajeTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 import dev.celia.vehiculo.Coche;
+import dev.celia.vehiculo.Moto;
 import dev.celia.vehiculo.Vehiculo;
 import dev.celia.estacionpeaje.EstacionPeaje;
 
@@ -18,4 +19,11 @@ public class EstacionPeajeTest {
         assertEquals(100.00, estacion.getTotalRecaudado());
     }
 
+    @Test
+    void testRegistrarVehiculoMoto(){
+        EstacionPeaje estacion = new EstacionPeaje("Peaje 3", "León");
+        Vehiculo moto = new Moto("1234DEF");
+        estacion.registrarVehículo(moto);
+        assertEquals(50.00, estacion.getTotalRecaudado());
+    }
 }
